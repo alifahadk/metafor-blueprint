367a17baa328903be2e057432f8b69eeb0b4f49e
alifahadk <alifahadkhanlodhi@gmail.com>
2025-07-20 02:49:31 +0200

    add experiment dsl files

---
+++ experiment-dsl-150.py
@@ -0,0 +1,11 @@
+from metafor.dsl.dsl import Server, Work, Source, Program, DependentCall, Constants
+from metafor.analysis.visualize import Visualizer
+
+api = { 'insert ': Work (10 , [] ,) }
+
+server = Server ('simple', api, qsize=150, thread_pool=1)
+src = Source ('client', 'insert', rate=5, timeout=5, retries=5)
+
+
+p = Program('SimpleService')
+p.add_server(server).add_source(src).connect ('client', 'simple')
